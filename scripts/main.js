548691affd2268421b0e5af4d4d951695c668c9a
I-FIEVRE <fievre.isabelle@gmail.com>
2023-04-18 20:46:42 +0200

    task1

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const lastUpdated = new Date(document.lastModified);
+document.querySelector('#lastUpdated').textContent = lastUpdated.toLocaleDateString() + ' ' + lastUpdated.toLocaleTimeString();
+const currentDate = new Date();
+document.querySelector('#currentyear').textContent = currentDate.getFullYear();
