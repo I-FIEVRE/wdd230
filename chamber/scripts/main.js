ec09f130bc009d46ee8e007fdb713991bfde031f
I-FIEVRE <fievre.isabelle@gmail.com>
2023-05-11 19:42:18 +0200

    home page

---
+++ chamber/scripts/main.js
@@ -0,0 +1,19 @@
+const lastUpdated = new Date(document.lastModified);
+document.querySelector('#lastUpdated').textContent = `${lastUpdated.toLocaleDateString()} ${lastUpdated.toLocaleTimeString()}`;
+const currentDate = new Date();
+document.querySelector('#currentyear').textContent = currentDate.getFullYear();
+/*document.querySelector('#currentdate').textContent = `${currentDate.getDay()}, `;*/
+const date = document.querySelector("#currentdate");
+try {
+    const option = {
+        weekday: "long"
+    }
+	const options = {
+		month: "long",
+		day: "numeric",
+		year: "numeric"
+	};
+	date.innerHTML = `${new Date().toLocaleDateString("en-UK", option)}, ${new Date().toLocaleDateString("en-UK", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
