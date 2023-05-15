8985e6600de76f9f2fad303d1bf3e055eb3dbd26
I-FIEVRE <fievre.isabelle@gmail.com>
2023-05-15 21:42:42 +0200

    activity lesson5

---
+++ lesson5/scripts/main.js
@@ -0,0 +1,22 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {const myItem = input.value;
+ input.value = '';                                
+const item = document.createElement('li');
+const text = document.createElement('span');
+const btn = document.createElement('button');
+                                        
+item.appendChild(text);
+text.textContent = myItem;
+item.appendChild(btn);                 
+btn.textContent = 'Delete';
+list.appendChild(item);
+
+btn.addEventListener('click', () => {
+  list.removeChild(item);               
+});
+
+input.focus();                         
+});
