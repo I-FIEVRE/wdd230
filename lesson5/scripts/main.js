862d0f5c1a2f223ed2fe5b30b9bd4988f81dcbaa
I-FIEVRE <fievre.isabelle@gmail.com>
2023-05-16 10:14:07 +0200

    case  without writing

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -2,20 +2,21 @@ const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => {const myItem = input.value;
- input.value = '';                                
-const item = document.createElement('li');
-const text = document.createElement('span');
-const btn = document.createElement('button');
-                                        
-item.appendChild(text);
-text.textContent = myItem;
-item.appendChild(btn);                 
-btn.textContent = 'Delete';
-list.appendChild(item);
-
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';                                
+  const item = document.createElement('li');
+  const text = document.createElement('span');
+  const btn = document.createElement('button');
+  if (myItem != '') {
+    item.appendChild(text);
+    text.textContent = myItem;
+    item.appendChild(btn);                 
+    btn.textContent = 'Delete';
+    list.appendChild(item);
+  } 
 btn.addEventListener('click', () => {
-  list.removeChild(item);               
+  list.removeChild(item);             
 });
 
 input.focus();                         
