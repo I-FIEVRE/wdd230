6ede870bcf8ba2398e75411a77776f9eb0896cf9
I-FIEVRE <fievre.isabelle@gmail.com>
2023-05-25 08:45:27 +0200

    changes pictures rivoli

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -9,4 +9,4 @@ const windS = windSpeed * 0.6213711922;
 if (temp <= 50 && windS > 3 ) {
     const windC = 35.74 + .06215 * temp - 35.75 * windS ** 0.16 + 0.4275 * temp * windS ** 0.16;
     windChill.textContent = windC.toFixed(2);
-} else { windChill.textContent = 'N/A';}
+} else {windChill.textContent = 'N/A';}
