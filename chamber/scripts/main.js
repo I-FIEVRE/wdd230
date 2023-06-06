9566876f0e55ebe220f11fe971015d92bb26c87a
I-FIEVRE <fievre.isabelle@gmail.com>
2023-06-06 19:19:16 +0200

    join page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -31,3 +31,23 @@ if (currentDate.getDay() == 1 || currentDate.getDay() == 2) {
 		divi.style.display = "none";
 	})
 } else {divi.style.display = "none";}
+
+const today = new Date().getTime();
+const daysDisplay = document.querySelector(".days");
+
+let lastDayVisit = Number(window.localStorage.getItem("lastDayVisit-ls")) || 0;
+
+const diffTime = today - lastDayVisit;
+const diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+if (diffTime !== today) {
+	if (diffDays.toFixed(0) == '0' || diffDays.toFixed(0) =='1') {
+		daysDisplay.textContent = `It's been ` + diffDays.toFixed(0) + ` day since your last visit.`;
+	} else {
+		daysDisplay.textContent = `It's been ` + diffDays.toFixed(0) + ` days since your last visit.`;
+	}
+} else {
+	daysDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+localStorage.setItem("lastDayVisit-ls", today);
+
