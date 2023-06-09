48600f406be30a3f9503ab83a7a7b5ceae245262
I-FIEVRE <fievre.isabelle@gmail.com>
2023-06-10 00:56:44 +0200

    directory continue

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -13,8 +13,7 @@ async function getDirectoryData(url) {
     const gridD = document.querySelector('div.gridD');
     businesses.forEach((business) => {
       let card = document.createElement('section');
-      let divi1 = document.createElement('div');
-      let divi2 = document.createElement('div');
+      let divi = document.createElement('div');
       let h3 = document.createElement('h3');
       let image = document.createElement('img');
       let phone = document.createElement('p');
@@ -31,12 +30,11 @@ async function getDirectoryData(url) {
       membership.textContent = `${business.membership}`;
       card.appendChild(h3);
       card.appendChild(image);
-      card.appendChild(divi1);
-      divi1.appendChild(phone);
-      divi1.appendChild(address);
-      divi1.appendChild(website);
-      card.appendChild(divi2);
-      divi2.appendChild(membership);
+      card.appendChild(phone);
+      card.appendChild(address);
+      card.appendChild(website);
+      card.appendChild(divi);
+      divi.appendChild(membership);
       gridD.appendChild(card);
     })
   } 
@@ -45,6 +43,7 @@ async function getDirectoryData(url) {
     const listD = document.querySelector('div.listD');
     businesses.forEach((business) => {
       let card = document.createElement('section');
+      let divi = document.createElement('div');
       let h3 = document.createElement('h3');
       let phone = document.createElement('p');
       let address = document.createElement('p');
@@ -59,13 +58,14 @@ async function getDirectoryData(url) {
       card.appendChild(phone);
       card.appendChild(address);
       card.appendChild(website);
-      card.appendChild(membership);
+      card.appendChild(divi);
+      divi.appendChild(membership);
       listD.appendChild(card);
     })
   } 
 
-let viewsButtons = document.querySelectorAll('.links ul li');
-let views = document.querySelectorAll('.view-div');
+let viewsButtons = document.querySelectorAll('.buttons ul li');
+let views = document.querySelectorAll('.views div');
 
 viewsButtons.forEach((link) => {
   link.addEventListener('click', () => {
@@ -73,14 +73,14 @@ viewsButtons.forEach((link) => {
           item.classList.remove('active');
       })
       link.classList.add('active');
-      let li_view = link.getAttribute('data-view');
+      let dataCard = link.getAttribute('data-card');
       views.forEach((view) => {
           view.style.display = 'none';
       }) 
-      if ((li_view == 'gridD')) {
-        document.querySelector('.' + li_view).style.display = 'grid';
+      if ((dataCard == 'gridD')) {
+        document.querySelector('.' + dataCard).style.display = 'grid';
       } else {
-        document.querySelector('.' + li_view).style.display = 'block';
+        document.querySelector('.' + dataCard).style.display = 'block';
       }
         
   })
