570e5b07b7b9122fd284f87d9b0dacf5b9a96867
I-FIEVRE <fievre.isabelle@gmail.com>
2023-06-30 16:55:32 +0200

    error

--- chamber/scripts/main2.js
+++ chamber/scripts/main2.js
@@ -29,7 +29,7 @@ const diffTime = today - lastDayVisit;
 const diffDays = diffTime / (1000 * 60 * 60 * 24);
 
 if (diffTime !== today) {
-	if (diffDays.toFixed(0) == 0 || diffDays.toFixed(0) == 1 ) {
+	if (diffDays.toFixed(0) == '0' || diffDays.toFixed(0) =='1') {
 		daysDisplay.textContent = `It's been ` + diffDays.toFixed(0) + ` day since your last visit.`;
 	} else {
 		daysDisplay.textContent = `It's been ` + diffDays.toFixed(0) + ` days since your last visit.`;
